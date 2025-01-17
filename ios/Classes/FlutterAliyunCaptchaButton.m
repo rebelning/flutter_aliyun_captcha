4ebf7cddc496644f9732ea2964018a24df1e891d
rebelning <shaxiaoan@gmail.com>
2025-01-17 15:37:41 +0800

    aliyun captcha

--- ios/Classes/FlutterAliyunCaptchaButton.m
+++ ios/Classes/FlutterAliyunCaptchaButton.m
@@ -40,11 +40,11 @@
         _aliyunCaptchaButton.onSuccess = ^(NSString * _Nonnull data) {
 //            NSString *dataString = [data description];
 //            NSLog(@"_aliyunCaptchaButton.onSuccess  dataString %@", dataString);
-            NSString *quotedDataString = [NSString stringWithFormat:@"%@", data ?: @""];
+//            NSString *quotedDataString = [NSString stringWithFormat:@"%@", data ?: @""];
 //            NSLog(@"_aliyunCaptchaButton.onSuccess quotedDataString %@", quotedDataString);
             NSDictionary<NSString *, id> *result = @{
                 @"method": @"onSuccess",
-                @"data": quotedDataString,
+                @"data": data ?: @"",
             };
 //            self->_eventSink(eventData);
             [weakSelf notifyFlutterAndAwaitResult:@"onSuccess" data:result isCallback:YES];
